b713ca03ba38a1e3aadb8aaf5f3bae85ef106d4f
beckyjobita <122250572+BeckyJobita@users.noreply.github.com>
2023-11-25 10:23:04 +0300

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+function sendEmail() {
+    window.open("https://www.gmail.com/", "_blank");
+}
+
+function openCard() {
+    window.open("https://price-card-six.vercel.app/", "_blank");
+}
+
+function openTestimonial() {
+    window.open("https://beckyjobita.github.io/testimonialGrid/", "_blank");
+}
+
+function linkedIn() {
+    window.open("https://www.linkedin.com/in/joy-olonga-202540229/", "_blank");
+}
+
+function gitHub() {
+    window.open("https://github.com/BeckyJobita", "_blank");
+}
